4a719c05d0ac93ced0b893ee86eb67beb2339037
Wanderthedog <jmichajlovicova@seznam.cz>
2024-02-11 22:37:29 +0100

    snehulak+zmrzlina, masinka zvlast, odstranení zbytecnych presunu

--- src/main/java/cz/czechitas/turtle/HlavniProgram.java
+++ src/main/java/cz/czechitas/turtle/HlavniProgram.java
@@ -13,149 +13,156 @@ public class HlavniProgram {
 
     public void start() {
 
-        //nakresliSnehulaka(new Color(255, 196, 0));
-        nakresliMasinku();
-        //nakresliZmrzlinu(Color.cyan);
-
-        // vypocitejDelkuPrepony();
+       nakresliSnehulaka(new Color(255, 0, 255));
+       nakresliZmrzlinu(Color.yellow);
+      // nakresliMasinku();
     }
-
-    public void nakresliZmrzlinu(Color barva) {
+        public void nakresliZmrzlinu(Color barva) {
+        poziceProKresleniZmrliny();
         zofka.setPenColor(barva);
-        // nakresliKolečko(100);
-        zofkaNaVychoziPozici();
-        nakresliRovnoramennyTrojuhelnik(424.264, 300, Color.CYAN);
+        nakresliKolecko(52.09);
+        zofka.penUp();
+        zofka.turnLeft(90);
+        zofka.move(30);
+        zofka.turnLeft(90);
+        zofka.move(52.09);
+        zofka.turnRight(180);
+        zofka.penDown();
+        zofka.turnLeft(90);
+        nakresliRovnoramennyTrojuhelnikZmrzlina(104.189,300, new Color(165,42,42));
+        zofka.setPenColor(barva);
     }
 
-    public void nakresliObdelnik(double delkaStranyA, double delkaStranyB, Color barva) {
+    public void nakresliRovnoramennyTrojuhelnikZmrzlina(double delkaStranyA, double delkaStranyB, Color barva){
         zofka.setPenColor(barva);
         zofka.turnRight(90);
+        zofka.move(delkaStranyA);
+        zofka.turnRight(100);
+        zofka.move(delkaStranyB);
+        zofka.turnRight(160);
+        zofka.move(delkaStranyB);
+    }
+    public void nakresliObdelnik(double delkaStranyA, double delkaStranyB, Color barva) {
+        zofka.setPenColor(barva);
         for (double i = 0; i < 2; i++) {
             zofka.turnLeft(90);
             zofka.move(delkaStranyB);
             zofka.turnLeft(90);
             zofka.move(delkaStranyA);
-
         }
-        zofka.penUp();
-        zofka.move(200);
-        zofka.turnLeft(90);
-        zofka.penDown();
     }
-
-    public void nakresliKoleckoRuce(double polomer) {
-        //zofka.penUp();
-        //zofka.turnLeft(35);
-        //zofka.move(460);
-        //zofka.penDown();
-        zofka.turnRight(90);
-        int pocetCasti = 360; // 360 casti
-        for (int i = 0; i < pocetCasti; i++) {
-            zofka.move(polomer * 3.14 * 2 / pocetCasti); // 360x
-            zofka.turnLeft(1); // Otoci se o jeden stupen
-        }
+    public void nakresliKoleckoRuce(Color barva) {
+        zofka.setPenColor(barva);
+        nakresliKolecko(60);
+        zofka.turnLeft(90);
         zofka.penUp();
+        zofka.move(60);
         zofka.turnLeft(90);
-        zofka.move(polomer);
-        zofka.turnLeft(90);
-        zofka.move(polomer);
+        zofka.move(60);
         zofka.penDown();
         nakresliKolecko(30);
-        zofka.turnRight(180);
+        zofka.turnRight(90);
         zofka.penUp();
-        zofka.move(polomer * 3);
+        zofka.move(120);
         zofka.penDown();
         nakresliKolecko(30);
-        zofka.turnRight(180);
-        zofka.penUp();
-        zofka.move(polomer * 2);
         zofka.turnRight(90);
-        zofka.move(polomer);
+        zofka.penUp();
+        zofka.move(60);
+        zofka.turnRight(90);
+        zofka.move(60);
         zofka.penDown();
     }
 
     public void nakresliSnehulaka(Color barva) {
-        zofkaNaVychoziPozici();
+        poziceProKresleniSnehulaka();
         zofka.setPenColor(barva);
         nakresliKolecko(90);
-        nakresliKoleckoRuce(60);
+        zofka.turnRight(90);
+        nakresliKoleckoRuce(Color.blue);
+        zofka.setPenColor(barva);
         nakresliKolecko(40);
     }
 
-    public void nakresliMasinku() {
-        zofkaNaVychoziPozici();
-        nakresliObdelnik(300, 150, Color.cyan);
-        nakresliObdelnik(200, 300, Color.green);
-        nakresliRovnoramennyTrojuhelnik(212.132, 150, Color.BLACK);
-        nakresliKoleckaLokomotivy(Color.cyan);
-    }
-
-    public void nakresliRovnoramennyTrojuhelnik(double delkaStranyA, double delkaStranyB, Color barva) {
-        zofka.setPenColor(barva);
+    public void poziceProKresleniSnehulaka(){
         zofka.penUp();
         zofka.turnLeft(90);
-        zofka.move(700);
-        zofka.turnRight(90);
-        zofka.move(100);
-        zofka.turnRight(135);
+        zofka.move(200);
+        zofka.turnLeft(90);
         zofka.penDown();
+    }
+    public void poziceProKresleniZmrliny(){
+        zofka.penUp();
+        zofka.move(300);
+        zofka.turnLeft(90);
+        zofka.penDown();
+    }
+    public void nakresliMasinku() {
+        zofkaPosunProNakresMasinky();
         zofka.turnRight(90);
+        nakresliObdelnik(300, 150, Color.cyan);
+        zofka.penUp();
+        zofka.move(200);
+        zofka.penDown();
+        nakresliObdelnik(200, 300, Color.green);
+        zofka.penUp();
+        zofka.turnRight(180);
+        zofka.penDown();
+        nakresliKolecko(100);
+        zofka.penUp();
+        zofka.turnLeft(90);
+        zofka.move(300);
+        zofka.turnLeft(90);
+        zofka.penDown();
+        nakresliKolecko(40);
+        zofka.penUp();
+        zofka.move(100);
+        zofka.turnLeft(90);
+        zofka.penDown();
+        nakresliKolecko(40);
+        zofka.penUp();
+        zofka.move(100);
+        zofka.turnRight(90);
+        zofka.move(60);
+        zofka.turnLeft(135);
+        zofka.penDown();
+        nakresliRovnoramennyTrojuhelnikPravyUhel(141.421d, 100,Color.BLACK);
+    }
+
+    public void nakresliRovnoramennyTrojuhelnikPravyUhel(double delkaStranyA, double delkaStranyB, Color barva) {
+        zofka.setPenColor(barva);
         zofka.move(delkaStranyA);
         zofka.turnLeft(135);
         zofka.move(delkaStranyB);
         zofka.turnLeft(90);
         zofka.move(delkaStranyB);
-        zofka.turnRight(180);
-        zofka.move(130);
-
-    }
-
-    public void nakresliKoleckaLokomotivy(Color barva) {
-        zofka.setPenColor(barva);
-        zofka.penUp();
-        zofka.turnLeft(90);
-        zofka.move(110);
-        zofka.penDown();
-        nakresliKolecko(30);
-        zofka.penUp();
-        zofka.move(50);
-        zofka.penDown();
-        nakresliKolecko(30);
-        zofka.penUp();
-        zofka.move(120);
-        zofka.turnRight(90);
-        zofka.move(50);
-        zofka.turnRight(180);
-        zofka.penDown();
-        nakresliKolecko(100);
     }
 
     public void nakresliKolecko(double polomer) {
-        //zofka.penUp();
-        //zofka.turnLeft(35);
-        //zofka.move(460);
-        //zofka.penDown();
         zofka.turnRight(90);
         int pocetCasti = 360; // 360 casti
         for (int i = 0; i < pocetCasti; i++) {
             zofka.move(polomer * 3.14 * 2 / pocetCasti); // 360x
             zofka.turnLeft(1); // Otoci se o jeden stupen
         }
-        zofka.penUp();
-        zofka.turnLeft(90);
-        zofka.move(polomer * 2);
-        zofka.penDown();
     }
-
     public double vypocitejDelkuPrepony() {
         double delkaPrepony;
-        double velikostStrany = 300;
+        double velikostStrany = 100;
         delkaPrepony = Math.sqrt(2 * Math.pow(velikostStrany, 2));
         System.out.println(delkaPrepony);
         return delkaPrepony;
     }
 
-    public void zofkaNaVychoziPozici() {
+    public double vypocitejDelkuTretiStrany(double velikostRamene, double uhelMeziRameny) {
+        double tretiStrana;
+        tretiStrana = Math.abs((velikostRamene
+                * Math.sin((uhelMeziRameny * Math.PI / 180.0) / 2))
+                * 2);
+        System.out.println(tretiStrana);
+        return tretiStrana;
+    }
+    public void zofkaPosunProNakresMasinky() {
         zofka.penUp();
         zofka.turnLeft(180);
         zofka.move(100);
